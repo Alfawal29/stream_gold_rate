4dcebc7b5913416b086f88152c321f9f547e0657
Ammar <ammar.alfawal@gmx.de>
2025-03-24 11:42:47 +0100

    ZufallZahl

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
@@ -21,8 +22,28 @@ class GoldScreen extends StatelessWidget {
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
+              StreamBuilder(
+                stream: getGoldPriceStream(),  
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+
+                    return const CircularProgressIndicator();
+                    
+                  } else if 
+                    (snapshot.hasError){
+                      
+                     return Text('Fehler : ${snapshot.error}');
+                  }else if (snapshot.hasData){
+                  return Text(NumberFormat.simpleCurrency(locale: 'de_DE').format(snapshot.data),style: Theme.of(context).textTheme.headlineLarge!.copyWith(color: Theme.of(context).colorScheme.primary),);
+           
+
+
+                  } else {
+                    return const Text("Keine Daten Verfugbar");
+                  }
+
+                },),
+
               Text(
                 NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
                 style: Theme.of(context)
